5c7f8ad2f4e8836fdec55492f0f026248e7baf16
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-07-18 19:49:03 -0600

    Leccion 6: Calculo figuras geometricas

---
+++ taller1/figuras.js
@@ -0,0 +1,19 @@
+// Codigo del cuadrado
+console.group("Cuadrados")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado);
+console.groupEnd();
+
+// Codigo del triangulo
+console.group("Triangulo");
+const ladoTrianguloUno = 6;
+const ladoTrianguloDos = 6;
+const baseTriangulo = 4;
+console.log("Los lados del triangulo miden " + ladoTrianguloUno + "cm, " + ladoTrianguloDos + "cm, " + baseTriangulo + "cm");
+console.groupEnd();
