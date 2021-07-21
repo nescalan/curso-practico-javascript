c5ceb1a77b09df7007db41fd49aa655fc4bd77d4
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-07-20 19:18:31 -0600

    Leccion 15: funcion array.reduce()

---
+++ taller3/promedio.js
@@ -0,0 +1,18 @@
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //   sumaLista = sumaLista + lista[i];
+    // }
+  
+    const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+        console.log(`Valor Acumulado=${valorAcumulado} Nuevo Elemento=${nuevoElemento}`);
+
+        return valorAcumulado + nuevoElemento;
+      }
+    );
+  
+    const promedioLista = sumaLista / lista.length;
+  
+    return promedioLista;
+  }
